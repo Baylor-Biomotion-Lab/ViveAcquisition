828941842398ccbffc2c96d99bceae7cd7d3be70
Tyler Jost <32278047+TylerJost@users.noreply.github.com>
2018-10-16 10:14:48 -0500

    Final research version

--- controller_test.py
+++ controller_test.py
@@ -2,61 +2,19 @@ import triad_openvr
 import time
 import sys
 import matplotlib.pyplot as plt
-from drawnow import drawnow
-from mpl_toolkits import mplot3d
-from mpl_toolkits.mplot3d import Axes3D
+#import ctypes
+from threading import Thread
 
-#trialName = 'VR_SubjP03_Practice_Play_TR01'
-trialName = 'Test'
 
+#plotControllerCSV(trialName)
+
+
+#trialName = 'test'
+trialName = 'VR_VRValidation_RS9_'+'RRPFP'+'_TR'+'30'
 plt.close()
-# Make figures to update using subplot for yaw,pitch,roll
-def angle1():
-    plt.subplot(311)
-    plt.plot(frame,yaw1)
-    plt.title('Yaw')
-    plt.subplot(312)
-    plt.plot(frame,pitch1)
-    plt.title('Pitch')
-    plt.subplot(313)
-    plt.plot(frame,roll1)
-    plt.title('Roll')
-def position1():
-    plt.subplot(311)
-    plt.plot(frame,x1)
-    plt.title('X')
-    plt.subplot(312)
-    plt.plot(frame,y1)
-    plt.title('Y')
-    plt.subplot(313)
-    plt.plot(frame,z1)
-    plt.title('Z')
-def angle2():
-    plt.subplot(311)
-    plt.plot(frame,yaw2)
-    plt.title('Yaw')
-    plt.subplot(312)
-    plt.plot(frame,pitch2)
-    plt.title('Pitch')
-    plt.subplot(313)
-    plt.plot(frame,roll2)
-    plt.title('Roll')
-def position2():
-    plt.subplot(311)
-    plt.plot(frame,x2)
-    plt.title('X')
-    plt.subplot(312)
-    plt.plot(frame,y2)
-    plt.title('Y')
-    plt.subplot(313)
-    plt.plot(frame,z2)
-    plt.title('Z')
-def txt2str(txt):
-    txtNumList = [float(s) for s in txt.split()]
-    return txtNumList
 # Initialization for the controller code
 v = triad_openvr.triad_openvr()
-v.print_discovered_objects()
+v.print_discovered_objects() 
 
 if len(sys.argv) == 1:
     interval = 1/250
@@ -66,81 +24,114 @@ else:
     print("Invalid number of arguments")
     interval = False
     
-control1, control1txt = [],[]
-control2, control2txt = [],[]
-x1, x2 = [],[]
-y1, y2 = [],[]
-z1, z2 = [],[]
-yaw1, yaw2 = [],[]
-pitch1, pitch2 = [],[]
-roll1, roll2 = [],[]
-frame = []
+control1txt = []
+control2txt = []
+rtemps1 = []
+rtemps2 = []
+x= [] 
+y = []
+z = []
 tStart = time.time()
+
+def writeViveFiles():
+    control1Name = trialName+'control1.csv'
+    control2Name = trialName+'control2.csv'
+    control1RotMatName = trialName+'control1RotMat.csv'
+    control2RotMatName = trialName+'control2RotMat.csv'
+    tPassed = time.time()-tStart
+    print(tPassed)
+    # Check to see if we have exceeded the computational power required to keep this thing running > 200 Hz
+    if len(control1txt)<(tPassed*200):
+        print("Frame rate below 200 Hz, retry.")
+    # If we are recording correctly, write csv files with ridiculously long names
+    with open(control1Name,'w') as file:
+        for line in control1txt:
+            file.write(line)
+            file.write('\n')
+    with open(control2Name,'w') as file:
+        for line in control2txt:
+            file.write(line)
+            file.write('\n')
+    # Rotation matrices have to be extracted so they write nicely to .csv
+    with open(control1RotMatName,'w') as file:
+        for rt1 in rtemps1:
+            for row1 in rt1:
+                [file.write(str(row1[x])+',') if x<len(row1)-1 else file.write(str(row1[x])+'\n') for x in range(len(row1)) ]
+    with open(control2RotMatName,'w') as file:
+        for rt2 in rtemps2:
+            for row2 in rt2:
+                [file.write(str(row2[x])+',') if x<len(row2)-1 else file.write(str(row2[x])+'\n') for x in range(len(row2)) ]
+    
 if interval:
     try:
         i = 0
         while(True):
             start = time.time()
-            txt1 = ""
+            txt11 = ""
             # Loop and grab coordinates for controller 1
             for each in v.devices["controller_1"].get_pose_euler():
-                txt1 += "%.4f" % each
-                txt1 += " "
-            print("\rController 1: " + txt1, end="")
-            txt2 = ""
+                txt11 += "%.4f" % each
+                txt11 += " "
+            print("\rController 1: " + txt11, end="")
+            txt21 = ""
             print()
+            
             # Loop and grab coordinates for controller 2
             for each in v.devices["controller_2"].get_pose_euler():
-                txt2 += "%.4f" % each
-                txt2 += " "
-            print("\rController 2: " + txt2, end="\n")
+                txt21 += "%.4f" % each
+                txt21 += " "
+            print("\rController 2: " + txt21, end="\n")
             print()
-            # Add controller 1 and controller 2 coordinates to list
+            
+            txt12 = ""
+            for each in v.devices["controller_1"].get_pose_quaternion():
+                txt12 += "%.4f" % each
+                txt12 += " "
+            txt22 = ""
+            for each in v.devices["controller_2"].get_pose_quaternion():
+                txt22 += "%.4f" % each
+                txt22 += " "
+            
+            # Get pose matrices for controllers 1 and 2
+            rtemp1 = v.devices["controller_1"].get_RotMat()
+            rtemp2 = v.devices["controller_2"].get_RotMat()
             sleep_time = interval-(time.time()-start)
+            
             if sleep_time>0:
-                time.sleep(sleep_time)
-            frame.append(i)
+                time.sleep(sleep_time)           
+            
+            control1txt.append(txt11+txt12)
+            control2txt.append(txt21+txt22)
+            rtemps1.append(rtemp1)
+            rtemps2.append(rtemp2)
             
             
-            txt1Num = txt2str(txt1)
-            txt2Num = txt2str(txt2)
-            control1.append(txt1Num)
-            control2.append(txt2Num)
-            control1txt.append(txt1)
-            control2txt.append(txt2)
             
-            x1.append(control1[i][0])
-            y1.append(control1[i][1])
-            z1.append(control1[i][2])
-            
-            yaw1.append(control1[i][3])
-            pitch1.append((control1[i][4]))
-            roll1.append(control1[i][5])
-            
-            x2.append(control2[i][0])
-            y2.append(control2[i][1])
-            z2.append(control2[i][2])
-            
-            yaw2.append(control2[i][3])
-            pitch2.append((control2[i][4]))
-            roll2.append(control2[i][5])
-            i = i+1
-            # Plotting
-            drawnow(position1)
+            # Stop script if it runs more than 10 seconds
 #            tPassed = time.time()-tStart
 #            if tPassed>=10:
 #                raise KeyboardInterrupt('time"s up!')
     except KeyboardInterrupt:
         # When ctrl+c is pressed, there is a keyboard interrupt
         # Write each line into a .csv file
-        control1Name = trialName+'control1.csv'
-        control2Name = trialName+'control2.csv'
-        with open(control1Name,'w') as file:
-            for line in control1txt:
-                file.write(line)
-                file.write('\n')
-        with open(control2Name,'w') as file:
-            for line in control2txt:
-                file.write(line)
-                file.write('\n')
+        wF = Thread(target = writeViveFiles)
+        wF.start()
+        wF.join()
+        
+        x = [float(i.split()[0]) for i in control1txt]
+        y = [float(i.split()[1]) for i in control1txt]
+        z = [float(i.split()[2]) for i in control1txt]
+
+#        plt.figure(figsize = (13,7))
+#        plt.subplot(131)
+#        plt.plot(x)
+#        plt.subplot(132)
+#        plt.plot(y)
+#        plt.subplot(133)
+#        plt.plot(z)
+        if 0 in y:
+            print("Zeros detected, CHECK PLOT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        else:
+            print('No zeros detected')
+        
         print('Trial Ended')
